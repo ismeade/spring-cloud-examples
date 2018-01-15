ef40c14983e9f53237287d3e3f48f619c9f79579
liyang <ismeade99@sina.com>
2018-01-15 10:18:56 +0800

    up

--- eureka-server/src/main/java/com/ade/exp/spring/cloud/eureka/server/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/ade/exp/spring/cloud/eureka/server/EurekaServerApplication.java
@@ -1,5 +1,6 @@
 package com.ade.exp.spring.cloud.eureka.server;
 
+//import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -8,7 +9,15 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class EurekaServerApplication {
 
+    //public static Integer port;
+    //
+    //@Value("${server.port}")
+    //public void setPort(Integer port) {
+    //    EurekaServerApplication.port = port;
+    //}
+
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
-	}
+        //System.out.println(EurekaServerApplication.port);
+    }
 }
